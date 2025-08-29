d7027f13a62d4db617d07eda931e9ca2c3a687a8
AyyanNadeem01 <ayyan137a@gmail.com>
2025-08-30 03:06:03 +0500

    linearSearch and reverseArray

---
+++ linearSearch.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+bool LinearSearch(int arr[],int size,int n){
+    for(int i=0;i<size;i++){
+        if(arr[i]==n){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main (){
+
+    char iter='y';
+    do{
+        int Size;
+        cout<<"Enter Size of Array :";
+        cin>>Size;
+
+        int arr[Size];
+        
+        cout<<"Enter Elements of Array:";
+        for(int i=0;i<Size;i++){
+            cin>>arr[i];
+        }
+
+        cout<<"Enter Number to Search: ";
+        int n;
+        cin>>n;
+        if(LinearSearch(arr,Size,n)){
+            cout<<"Yes its found!"<<endl;
+        }else{
+            cout<<"Sorry Cannot found"<<endl;
+        }
+
+        cout<<"Enter N or n to Stop:";
+        cin>>iter;
+
+    }while(iter!='n'&& iter!='N');
+    return 0;
+}
