0e69cff10e02ad8d31e69c5d0352cabfe3cccbbb
AyyanNadeem01 <ayyan137a@gmail.com>
2025-11-14 01:49:38 +0500

    negative decimal to binary

--- bitwise/decimalToBinary.cpp
+++ bitwise/decimalToBinary.cpp
@@ -18,3 +18,51 @@ int main(){
     cout<<"Binary: "<<ans;
     return 0;
  }
+
+ //negative number to binary
+//  #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cout << "Enter a negative decimal number: ";
+//     cin >> n;
+
+//     if (n >= 0) {
+//         cout << "Please enter a negative number!" << endl;
+//         return 0;
+//     }
+
+//     int num = -n; // take absolute value
+//     vector<int> binary;
+
+//     // Step 1: Get binary of absolute value
+//     while (num > 0) {
+//         binary.push_back(num % 2);
+//         num /= 2;
+//     }
+
+//     // Pad to 8 bits (or 32 for full width)
+//     while (binary.size() < 8)
+//         binary.push_back(0);
+
+//     // Step 2: Invert bits
+//     for (int i = 0; i < binary.size(); i++)
+//         binary[i] = binary[i] == 0 ? 1 : 0;
+
+//     // Step 3: Add 1
+//     int carry = 1;
+//     for (int i = 0; i < binary.size(); i++) {
+//         int sum = binary[i] + carry;
+//         binary[i] = sum % 2;
+//         carry = sum / 2;
+//     }
+
+//     cout << "Binary (8-bit two's complement): ";
+//     for (int i = binary.size() - 1; i >= 0; i--)
+//         cout << binary[i];
+
+//     cout << endl;
+//     return 0;
+// }
