6c17afef6908b2842047669003692fbc8141ac28
AyyanNadeem01 <ayyan137a@gmail.com>
2025-11-14 01:42:00 +0500

    decimal to binary

--- bitwise/tempCodeRunnerFile.cpp
+++ bitwise/tempCodeRunnerFile.cpp
@@ -1,9 +1,1 @@
-    // int binary = 0b101011010; 
-    // int count=0;
-    // while(binary!=0){
-    //     if(binary&1){
-    //         count++;
-    //     }binary=binary>>1;
-    // } 
-
-    // cout<<"There are "<<count<<" ones in the given number."<<endl;
+2
