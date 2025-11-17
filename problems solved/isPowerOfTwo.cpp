91ede8f6d0c75eeb532b9cf9d6d63a5ef24599d5
AyyanNadeem01 <ayyan137a@gmail.com>
2025-11-17 19:58:36 +0500

    fixed isPowerOfTwo

--- problems solved/isPowerOfTwo.cpp
+++ problems solved/isPowerOfTwo.cpp
@@ -1,16 +1,19 @@
 #include<iostream>
 using namespace std;
-bool isPOwerOfTwo(int num){
-    int ans=1;
-    for(int i=0;i<=30;i++){
-         if(ans==num){
+bool isPowerOfTwo(int num) {
+    if(num <= 0) return false;
+    int ans = 1;
+    for(int i = 0; i <= 30; i++){
+        if(ans == num)
             return true;
-        }
-        if(ans<INT_MAX/2)
-        ans=ans*2;
-    return false;
+        if(ans < INT_MAX / 2)
+            ans *= 2;
+        else
+            break; 
     }
-}
+    return false; }
+
+
 int main(){
    
     return 0;
