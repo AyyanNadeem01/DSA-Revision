3fef29e5712e6621289f74d70e791c431873db8b
Ayyan Nadeem <ayyan137a@gmail.com>
2026-02-11 20:40:01 +0500

    insertionSort.cpp

--- sorting algos/insertionSort.cpp
+++ sorting algos/insertionSort.cpp
@@ -3,7 +3,7 @@ using namespace std;
 //insertion sort
 // it follow the approach of building a sorted array one element at a time
 //by moveing elements greater than the key to one position ahead
-void insertionSort(int arr[], int n) {
+void applyInsertionSort(int arr[], int n) {
     for (int i = 1; i < n; i++) {
         int key = arr[i];
         int j = i - 1;
@@ -24,7 +24,7 @@ int main() {
     int arr[] = {64, 25, 12, 22, 11};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    insertionSort(arr, n);
+    applyInsertionSort(arr, n);
 
     cout << "Sorted array: ";
     for (int i = 0; i < n; i++) {
